179463df32ab4bffd5deeb861a959176eb4ba7ea
Your Name <tbengtson82@gmail.com>
2021-10-29 12:03:39 -0500

    tabs.js set up

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,28 +1,67 @@
+import axios from "axios";
 const Tabs = (topics) => {
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
+
+    const topics_div = document.createElement("div");
+    topics_div.classList.add("topics");
+
+    topics.forEach(topic => {
+        const div = document.createElement("div");
+        div.classList.add("tab");
+        div.textContent = topic;
+        topics_div.appendChild(div);
+
+    })
+    return topics_div
 }
 
 const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+
+    axios.get(`http://localhost:5000/api/topics`)
+        .then(res => {
+            const topics = res.data.topics;
+            const tabs = document.querySelector(`${selector}`)
+            tabs.append(Tabs(topics))
+        })
+        .catch(err => {
+            console.error(err)
+        })
 }
 
 export { Tabs, tabsAppender }
+
+
+// TASK 3
+// ---------------------
+// Implement this function which takes an array of strings ("topics") as its only argument.
+// As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
+// then the function returns the markup below.
+// The tags used, the hierarchy of elements and their attributes must match the provided markup!
+// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+//
+// <div class="topics">
+//   <div class="tab">javascript</div>
+//   <div class="tab">bootstrap</div>
+//   <div class="tab">technology</div>
+// </div>
+//
+
+// TASK 4
+// ---------------------
+// Implement this function which takes a css selector as its only argument.
+// It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
+// Find the array of topics inside the response, and create the tabs using the Tabs component.
+// Append the tabs to the element in the DOM that matches the selector passed to the function.
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
